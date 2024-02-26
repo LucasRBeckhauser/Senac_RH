dfef67c0bffb2b8a406fa24eda8f4a2a8ea6b641
Lucas Roque Beckhauser <127861163+LucasRBeckhauser@users.noreply.github.com>
2024-02-26 20:46:38 -0300

    inicial

---
+++ PortalRh/src/main/java/br/com/portalRH/PortalRh/PortalRhApplication.java
@@ -0,0 +1,13 @@
+package br.com.portalRH.PortalRh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalRhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalRhApplication.class, args);
+	}
+
+}
---
+++ PortalRh/src/test/java/br/com/portalRH/PortalRh/PortalRhApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.portalRH.PortalRh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalRhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
