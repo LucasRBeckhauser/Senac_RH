c5f84bf425fc93fc15175b2d5f99753972d065fd
Lucas Roque Beckhauser <127861163+LucasRBeckhauser@users.noreply.github.com>
2024-02-27 19:59:15 -0300

    Cargo e certificações (falta terminar esse).

---
+++ PortalRh/src/main/java/br/com/portalRH/PortalRh/model/Cargo.java
@@ -0,0 +1,52 @@
+package br.com.portalRH.PortalRh.model;
+
+public class Cargo {
+
+    private String descricao;
+    private String nivel;
+    private Boolean comissionado;
+
+
+    // Getters and Setters
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(String nivel) {
+        this.nivel = nivel;
+    }
+
+    public Boolean getComissionado() {
+        return comissionado;
+    }
+
+    public void setComissionado(Boolean comissionado) {
+        this.comissionado = comissionado;
+    }
+
+    // Constructor vazio
+
+    public Cargo() {
+    }
+
+
+    // To string
+
+    @Override
+    public String toString() {
+        return "Cargo{" +
+                "descricao='" + descricao + '\'' +
+                ", nivel='" + nivel + '\'' +
+                ", comissionado=" + comissionado +
+                '}';
+    }
+
+}
---
+++ PortalRh/src/main/java/br/com/portalRH/PortalRh/model/Certificacoes.java
@@ -0,0 +1,10 @@
+package br.com.portalRH.PortalRh.model;
+
+import java.time.LocalDate;
+
+public class Certificacoes {
+
+    private String nomeCertificação;
+    private LocalDate dataEmissao;
+    private Double cargaHoraria;
+}
