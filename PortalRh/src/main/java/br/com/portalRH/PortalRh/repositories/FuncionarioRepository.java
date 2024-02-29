4c36709540f2cb4538922d7cf23520e4129db030
Lucas Roque Beckhauser <127861163+LucasRBeckhauser@users.noreply.github.com>
2024-02-28 21:44:42 -0300

    Criação do FuncionárioRepository e do FuncionarioService

--- PortalRh/src/main/java/br/com/portalRH/PortalRh/repositories/FuncionarioRepository.java
+++ PortalRh/src/main/java/br/com/portalRH/PortalRh/repositories/FuncionarioRepository.java
@@ -1,2 +1,10 @@
-package br.com.portalRH.PortalRh.repositories;public interface FuncionarioRepository {
+package br.com.portalRH.PortalRh.repositories;
+
+import br.com.portalRH.PortalRh.model.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FuncionarioRepository extends JpaRepository <Funcionario, Long> {
+
 }
--- PortalRh/src/main/java/br/com/portalRH/PortalRh/services/FuncionarioService.java
+++ PortalRh/src/main/java/br/com/portalRH/PortalRh/services/FuncionarioService.java
@@ -1,2 +1,43 @@
-package br.com.portalRH.PortalRh.services;public class FuncionarioService {
+package br.com.portalRH.PortalRh.services;
+
+import br.com.portalRH.PortalRh.model.Funcionario;
+import br.com.portalRH.PortalRh.repositories.FuncionarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class FuncionarioService {
+    @Autowired
+    private FuncionarioRepository repository;
+
+    public Funcionario salvar(Funcionario entity) {
+
+
+        // Regras de negócio
+
+        return repository.save(entity);
+    }
+
+    public List<Funcionario> trazerTodos() { return repository.findAll(); }
+
+    public Funcionario trazerporId(Long id) {return repository.findById(id).orElse(null);}
+
+    public void removerFuncionario(Long id) {repository.deleteById(id);}
+
+    public Funcionario alterarFuncionario(Long id, Funcionario entity) {
+        Optional<Funcionario> porId = repository.findById(id);
+
+        if(porId.isPresent()){
+            Funcionario funcionario = porId.get();
+
+            //funcionario.setAtributo(entity.geAtributo()); (Adaptar)
+
+            return repository.save(funcionario);
+        }
+        return null;
+
+    }
 }
