3e68262d29c8406d0b946013025b36d99ffa9b55
Lucas Roque Beckhauser <127861163+LucasRBeckhauser@users.noreply.github.com>
2024-02-29 20:58:18 -0300

    Criação do ValidationException e do AbstractController

---
+++ PortalRh/src/main/java/br/com/portalRH/PortalRh/enterprise/ValidationException.java
@@ -0,0 +1,9 @@
+package br.com.portalRH.PortalRh.enterprise;
+
+public class ValidationException extends RuntimeException {
+
+    public ValidationException(String message) {
+        super(message);
+    }
+}
+
